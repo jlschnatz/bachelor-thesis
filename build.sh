335fbdf5df13458874188819f4726de7d32a1013
Schnatz <janluca.schnatz@gmx.de>
2024-04-13 16:01:06 +0200

    build bash script echo on error helpful information

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ rm -r ${DIR}
 mkdir ${DIR}
 quarto render --quiet
 
-# remove unnecessary folders that are rendered
-mv ${DIR}/scripts/*.pdf ${DIR}
-rm -r ${DIR}/scripts
-rm -r ${DIR}/figures
+# remove unnecessary folders that are rendered (fails silently)
+mv ${DIR}/scripts/*.pdf ${DIR} 2>/dev/null
+rm -r ${DIR}/scripts 2>/dev/null
+rm -r ${DIR}/figures 2>/dev/null
 
 # rename pdf with current date and time
 DATE=$(date +"%d-%m-%Y")
@@ -18,4 +18,10 @@ TIME=$(date +"%H-%M")
 FILENAME="thesis-${AUTHOR}_${TIME}_${DATE}.pdf"
 mv ${DIR}/*.pdf "${DIR}/${FILENAME}"
 
-echo "Output created in ${DIR}/${FILENAME}"
+# echo sucess / fail information on exit
+if [ $(ls ${DIR} | wc -l) -eq 0 ]; then
+    LOG_FILE=$(find scripts -name "*.log" -type f)
+    echo "An error occurred in the compilation of the document, please see ${LOG_FILE}"
+else
+    echo "Output created in ${DIR}/${FILENAME}"
+fi
