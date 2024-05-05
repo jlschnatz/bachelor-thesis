974780e33a6949397a295b43c618d8c96b54d80a
Schnatz <janluca.schnatz@gmx.de>
2024-05-05 18:38:20 +0200

    create tidy bib file bash function

--- build.sh
+++ build.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
+# Tidy bib-file (only used references)
+clean_bib() {
+    folder="scripts"
+    input_ref="references.bib"
+    output_ref="tidy_references.bib"
+
+    # Extract unique entry names from files in the folder
+    entries=$(cat "$folder"/* | grep -o '@[a-zA-Z0-9_-]\+' | sed 's/@//g' | sort -u)
+
+    output=""
+    # Iterate over each entry and find the first occurrence in the bibliography
+    for entry in $entries; do
+        result=$(awk -v entry="$entry" 'BEGIN {RS="\n@"} $0 ~ entry {print "@" $0}' "$input_ref")
+        if [ -n "$result" ]; then
+            output+="$result\n"
+        fi
+    done
+
+    # Write the tidy references to the output file
+    echo -e "$output" > "$output_ref"
+}
+
+clean_bib
+
 # render project
 DIR="manuscript"
 AUTHOR="schnatz"
 rm -r ${DIR}
 mkdir ${DIR}
+
 quarto render --quiet
 
 # remove unnecessary folders that are rendered (fails silently)
